04a1f9e13d7f3edb8abb9df971e8f6bca67ec875
Vikas G J <vikasgj603@gmail.com>
2025-06-24 17:03:10 +0530

    Updated Code

--- cricrecords/settings.py
+++ cricrecords/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 from pathlib import Path
 
+import dj_database_url
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -88,10 +89,7 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'default': dj_database_url.config()
 }
 
 
