40dd810475ad7916bd3ebd0a7eafe0be304404ec
Vikas G J <vikasgj603@gmail.com>
2025-06-24 16:32:19 +0530

    Updated Code

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+
 pip install -r requirements.txt
-python manage.py migrate
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 python manage.py collectstatic --noinput
