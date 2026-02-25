769625f8ac5e8d281882fdf642221814ffe8e100
Ubuntu <ubuntu@ip-172-31-29-156.eu-north-1.compute.internal>
2026-02-25 09:35:46 +0000

    allowhost changed

--- records/tests.py
+++ records/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+class HomePageTest(TestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
